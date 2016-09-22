976d238e094dbc4aa9bdd803e29cccd1939d6e08
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-22 11:31:06 -0700

    'CJK'

--- tpllib/__init__.py
+++ tpllib/__init__.py
@@ -7,6 +7,9 @@ import os.path
 
 from traitlets.config import Config
 from nbconvert.exporters.html import HTMLExporter
+from nbconvert.exporters.latex import LatexExporter
+
+__version__='0.1.2'
 
 #-----------------------------------------------------------------------------
 # Classes
@@ -17,12 +20,6 @@ class MyExporter(HTMLExporter):
     My custom exporter  
     """
     
-    # def _file_extension_default(self):
-    #     """
-    #     The new file extension is `.test_ext`
-    #     """
-    #     return '.test_ext'
-
     @property
     def template_path(self):
         """
@@ -36,3 +33,28 @@ class MyExporter(HTMLExporter):
         We want to use the new template we ship with our library.
         """
         return 'noprompt' # full
+
+
+
+class TexCJK(LatexExporter):
+    """
+    My custom exporter  
+    """
+    
+    @property
+    def template_path(self):
+        """
+        We want to inherit from HTML template, and have template under
+        `./templates/` so append it to the search path. (see next section)
+        """
+        return super().template_path+[os.path.join(os.path.dirname(__file__), "templates")]
+
+    def _template_file_default(self):
+        return 'noprompt' # full
+
+
+class ChineeseCJK(TexCJK):
+
+    def _template_file_default(self):
+        return 'latex_chineese'
+
